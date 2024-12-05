15effd4ab44b9d1b82ccbab6e5ba7ef6f1d798b5
Haye Marc <marc.haye@etu.umontpellier.fr>
2024-12-05 23:57:43 +0100

    feat: genericite lecture fichier

--- src/server/server.js
+++ src/server/server.js
@@ -3,20 +3,35 @@ const fs = require('fs').promises;
 
 const app = express();
 
-// src/vue/test.html
-
-// Ajouter une route de base
 app.get('/', async (req, res) => {
 
     try {
-      const data = await fs.readFile('./index.html', 'utf8'); 
-      res.send(data); 
+      res.send(await lireFichier('./index.html'));
     }
     catch (err) {
       console.error(err);
       res.status(500).send('Error reading file'); 
     }
 });
+
+app.get('/:fichier', async (req, res) =>{
+
+    const { fichier } = req.params;
+
+    if (fichier === "favicon.ico"){return;}
+
+    try {
+        res.send(await lireFichier(`../vue/${fichier}.html`));
+    }
+    catch (err){
+        console.error(err);
+        res.status(500).send('Error reading file');
+    }
+});
+
+async function lireFichier(url){
+    return await fs.readFile(url, 'utf8');
+}
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
